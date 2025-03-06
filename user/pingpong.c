1f9bddce49c6f57043a463830905fe87b14bab03
zsd <3086188896@qq.com>
2025-03-06 06:49:47 -0800

    第二次测试

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,39 +1,66 @@
 #include "kernel/types.h"
 #include "user/user.h"
-#include "kernel/stat.h"
 
-int main(int argc, char* argv[]){
-    int pp2c[2], pc2p[2];
-    pipe(pp2c);
-    pipe(pc2p);
+#define RD 0 //pipe的read端
+#define WR 1 //pipe的write端
 
-    if(fork()!=0){//父进程
-        //向管道中输入一个字符
-        write(pp2c[1], ".", 1);
-        close(pp2c[1]);//即使关闭输入管道
+int main(int argc, char const *argv[]) {
+    char buf = 'P'; //用于传送的字节
 
-        //父进程读入子进程的字符
-        char buf;
-        read(pc2p[0], &buf, 1);
-        printf("%d: received pong\n", getpid());
+    int fd_c2p[2]; //子进程->父进程
+    int fd_p2c[2]; //父进程->子进程
+    pipe(fd_c2p);
+    pipe(fd_p2c);
 
-        //等待结束
-        wait(0);
+    int pid = fork();
+    int exit_status = 0;
+
+    if (pid < 0) {
+        fprintf(2, "fork() error!\n");
+        close(fd_c2p[RD]);
+        close(fd_c2p[WR]);
+        close(fd_p2c[RD]);
+        close(fd_p2c[WR]);
+        exit(1);
+    } else if (pid == 0) { //子进程
+        close(fd_p2c[WR]);
+        close(fd_c2p[RD]);
+
+        if (read(fd_p2c[RD], &buf, sizeof(char)) != sizeof(char)) {
+            fprintf(2, "child read() error!\n");
+            exit_status = 1; //标记出错
+        } else {
+            fprintf(1, "%d: received ping\n", getpid());
+        }
+
+        if (write(fd_c2p[WR], &buf, sizeof(char)) != sizeof(char)) {
+            fprintf(2, "child write() error!\n");
+            exit_status = 1;
+        }
+
+        close(fd_p2c[RD]);
+        close(fd_c2p[WR]);
+
+        exit(exit_status);
+    } else { //父进程
+        close(fd_p2c[RD]);
+        close(fd_c2p[WR]);
+
+        if (write(fd_p2c[WR], &buf, sizeof(char)) != sizeof(char)) {
+            fprintf(2, "parent write() error!\n");
+            exit_status = 1;
+        }
+
+        if (read(fd_c2p[RD], &buf, sizeof(char)) != sizeof(char)) {
+            fprintf(2, "parent read() error!\n");
+            exit_status = 1; //标记出错
+        } else {
+            fprintf(1, "%d: received pong\n", getpid());
+        }
+
+        close(fd_p2c[WR]);
+        close(fd_c2p[RD]);
+
+        exit(exit_status);
     }
-    else{
-        //读入父进程的字符
-        char buf;
-        read(pp2c[0], &buf, 1);
-        printf("%d :received ping\n", getpid());
-
-        //向父进程写入一个字符
-        write(pc2p[1], &buf, 1);
-        close(pc2p[1]);
-    }
-
-    //关闭管道读端
-    close(pp2c[0]);
-    close(pc2p[0]);
-
-    exit(0);
 }
